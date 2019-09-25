d04c539483dfe1f8c41e5340f81824f32ff4ae5c
DuyN <duy210697@gmail.com>
2019-09-25 20:28:22 +0700

    update responsive

--- src/pages/BlogDetail.js
+++ src/pages/BlogDetail.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import marked from 'marked';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons'
 import { Base64 } from 'js-base64';
+import { Link } from "react-router-dom";
 
 import '../css/content.css';
 
@@ -10,7 +13,7 @@ marked.setOptions({
 });
 const renderer = new marked.Renderer();
 renderer.link = (href, title, text) => {
-  return `<a id="tribute-link" href=${href} title=${title}" target="_blank">${text}</a>`;
+    return `<a id="tribute-link" href=${href} title=${title}" target="_blank">${text}</a>`;
 };
 
 export default class BlogDetail extends React.Component {
@@ -41,9 +44,12 @@ export default class BlogDetail extends React.Component {
     }
 
     render() {
-        const markdownText = marked(this.state.content,renderer);
+        const markdownText = marked(this.state.content, renderer);
         return (
             <div className="custom-container custom-container__bg-raisin-black blog-content color-black-coral">
+                <Link className="link-back button-back" to={`/blog`}>
+                    <FontAwesomeIcon icon={faLongArrowAltLeft} />
+                </Link>
                 <div className="box-content" dangerouslySetInnerHTML={{ __html: markdownText }}>
 
                 </div>
