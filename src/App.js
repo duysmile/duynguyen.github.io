15fab8e14f57c5f1fd06e94a1a76c2979b7a129d
Duy Nguyen <duy210697@gmail.com>
2022-06-13 11:11:33 +0700

    remove home page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './containers/BlogContainer';
 import BlogDetail from './containers/BlogDetailContainer';
@@ -14,9 +13,8 @@ function App() {
     <Provider store={store}>
       <Router>
         <div>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Blog} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/blog" component={Blog} />
           <Route exact path="/blog/:id" component={BlogDetail} />
         </div>
       </Router>
